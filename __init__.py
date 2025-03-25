469fce24fd93d21cd250afa12a9cb4455aa34ff3
RoninDykerman <ronindykerman@gmail.com>
2025-03-25 16:57:51 -0400

    Inshallah

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ The flask application package.
 
 from flask import Flask
 app = Flask(__name__)
-import Test.views
+import views
--- views.py
+++ views.py
@@ -4,13 +4,13 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from Test import app
+from __init__ import app
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template(
-        'templates/home.html',
+        'home.html',
         title='Home Page',
         year=datetime.now().year,
     )
@@ -18,63 +18,60 @@ def home():
 @app.route('/library')
 def library():
     return render_template(
-        'templates/library.html',
+        'library.html',
         year=datetime.now().year,
     )
 
 @app.route('/hotel_california')
 def hotel_california():
     return render_template(
-        'templates/hotel_california.html'
+        'hotel_california.html'
     )
 
 @app.route('/hotel_california_legend')
 def hotel_california_legend():
     return render_template(
-        'templates/hotel_california_legend.html'
+        'hotel_california_legend.html'
     )
 
 @app.route('/thatll_be_the_day')
 def thatll_be_the_day():
     return render_template(
-        'templates/thatll_bethe_day.html'
+        'thatll_bethe_day.html'
     )
 
 @app.route('/la_bamba')
 def la_bamba():
     return render_template(
-        'templates/la_bamba.html'
+        'la_bamba.html'
     )
 
 @app.route('/test')
 def test():
     return render_template(
-        'templates/testSong.html'
+        'testSong.html'
     )
 
 @app.route('/let_it_be')
 def let_it_be():
     return render_template(
-        'templates/let_it_be.html'
+        'let_it_be.html'
     )
 
 @app.route('/take_it_easy')
 def take_it_easy():
     return render_template(
-        'templates/take_it_easy.html'
+        'take_it_easy.html'
     )
 
 @app.route('/come_on_lets_go')
 def come_on_lets_go():
     return render_template(
-        'templates/come_on_lets_go.html'
+        'come_on_lets_go.html'
     )
 
 @app.route('/tutorial')
 def tutorial():
     return render_template(
-        'templates/tutorial.html'
+        'tutorial.html'
     )
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
